0815ec24284c9372cd7de061463a639bfd57e310
hmshreyas7 <hmshreyas7@gmail.com>
2019-08-29 19:46:27 +0530

    Load images and preview

--- main.py
+++ main.py
@@ -5,3 +5,22 @@ import torch.optim as optim
 
 import torchvision
 import torchvision.transforms as transforms
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Load images and preview
+data_transform = transforms.Compose([
+    transforms.RandomCrop(224),
+    transforms.ToTensor()
+])
+exdark_dataset = torchvision.datasets.ImageFolder(root = "./data/ExDark", transform = data_transform)
+data_loader = torch.utils.data.DataLoader(dataset = exdark_dataset, batch_size = 10, shuffle = True)
+
+batch = next(iter(data_loader))
+images, labels = batch
+
+grid = torchvision.utils.make_grid(images, nrow = 10)
+plt.figure(figsize=(15,15))
+plt.imshow(np.transpose(grid, (1, 2, 0)))
+plt.show()
