2a0c0e99fd09fbfc83a508b1816cea77623854f2
hmshreyas7 <hmshreyas7@gmail.com>
2019-08-30 02:40:19 +0530

    Perform exploratory analysis

--- main.py
+++ main.py
@@ -8,13 +8,15 @@ import torchvision.transforms as transforms
 
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 # Load images and preview
 data_transform = transforms.Compose([
     transforms.RandomCrop(224),
     transforms.ToTensor()
 ])
-exdark_dataset = torchvision.datasets.ImageFolder(root = "./data/ExDark", transform = data_transform)
+data_root = "./data/ExDark"
+exdark_dataset = torchvision.datasets.ImageFolder(root = data_root, transform = data_transform)
 data_loader = torch.utils.data.DataLoader(dataset = exdark_dataset, batch_size = 10, shuffle = True)
 
 batch = next(iter(data_loader))
@@ -23,4 +25,16 @@ images, labels = batch
 grid = torchvision.utils.make_grid(images, nrow = 10)
 plt.figure(figsize=(15,15))
 plt.imshow(np.transpose(grid, (1, 2, 0)))
+plt.show()
+
+# Exploratory Analysis
+data_folders = os.listdir(data_root)
+classes = []
+class_sizes = []
+for folder in data_folders:
+    classes.append(folder)
+    class_files = os.listdir(data_root + "/" + folder)
+    class_sizes.append(len(class_files))
+
+plt.bar(classes, class_sizes)
 plt.show()
