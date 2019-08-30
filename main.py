46a0e280ae941009376a32091de28d806a6d7aea
hmshreyas7 <hmshreyas7@gmail.com>
2019-08-30 20:40:24 +0530

    Define YOLO network - layers

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ plt.figure(figsize=(15,15))
 plt.imshow(np.transpose(grid, (1, 2, 0)))
 plt.show()
 
-# Exploratory Analysis
+# Perform exploratory analysis
 data_folders = os.listdir(data_root)
 classes = []
 class_sizes = []
@@ -38,3 +38,45 @@ for folder in data_folders:
 
 plt.bar(classes, class_sizes)
 plt.show()
+
+# Define YOLO network
+class YOLO(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # Input size 448 x 448
+        self.conv1 = nn.Conv2d(in_channels = 3, out_channels = 64, kernel_size = 7, stride = 2)
+        # Input size 112 x 112
+        self.conv2 = nn.Conv2d(in_channels = 64, out_channels = 192, kernel_size = 3)
+        # Input size 56 x 56
+        self.conv3 = nn.Conv2d(in_channels = 192, out_channels = 128, kernel_size = 1)
+        self.conv4 = nn.Conv2d(in_channels = 128, out_channels = 256, kernel_size = 3)
+        self.conv5 = nn.Conv2d(in_channels = 256, out_channels = 256, kernel_size = 1)
+        self.conv6 = nn.Conv2d(in_channels = 256, out_channels = 512, kernel_size = 3)
+        # Input size 28 x 28
+        self.conv7 = nn.Conv2d(in_channels = 512, out_channels = 256, kernel_size = 1)
+        self.conv8 = nn.Conv2d(in_channels = 256, out_channels = 512, kernel_size = 3)
+        self.conv9 = nn.Conv2d(in_channels = 512, out_channels = 256, kernel_size = 1)
+        self.conv10 = nn.Conv2d(in_channels = 256, out_channels = 512, kernel_size = 3)
+        self.conv11 = nn.Conv2d(in_channels = 512, out_channels = 256, kernel_size = 1)
+        self.conv12 = nn.Conv2d(in_channels = 256, out_channels = 512, kernel_size = 3)
+        self.conv13 = nn.Conv2d(in_channels = 512, out_channels = 256, kernel_size = 1)
+        self.conv14 = nn.Conv2d(in_channels = 256, out_channels = 512, kernel_size = 3)
+        self.conv15 = nn.Conv2d(in_channels = 512, out_channels = 512, kernel_size = 1)
+        self.conv16 = nn.Conv2d(in_channels = 512, out_channels = 1024, kernel_size = 3)
+        # Input size 14 x 14
+        self.conv17 = nn.Conv2d(in_channels = 1024, out_channels = 512, kernel_size = 1)
+        self.conv18 = nn.Conv2d(in_channels = 512, out_channels = 1024, kernel_size = 3)
+        self.conv19 = nn.Conv2d(in_channels = 1024, out_channels = 512, kernel_size = 1)
+        self.conv20 = nn.Conv2d(in_channels = 512, out_channels = 1024, kernel_size = 3)
+        self.conv21 = nn.Conv2d(in_channels = 1024, out_channels = 1024, kernel_size = 3)
+        self.conv22 = nn.Conv2d(in_channels = 1024, out_channels = 1024, kernel_size = 3, stride = 2)
+        # Input size 7 x 7
+        self.conv23 = nn.Conv2d(in_channels = 1024, out_channels = 1024, kernel_size = 3)
+        self.conv24 = nn.Conv2d(in_channels = 1024, out_channels = 1024, kernel_size = 3)
+        
+        self.fc1 = nn.Linear(in_features = 1024 * 7 * 7, out_features = 4096)
+        self.out = nn.Linear(in_features = 4096, out_features = 7 * 7 * 30)
+
+    def forward(self, t):
+        # Implement forward pass here
+        return t
